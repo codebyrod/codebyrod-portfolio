c7e0a7f4a04c3e8642451214bba04af99c7b5c16
codebyrod <rodrigos.comunica@gmail.com>
2022-10-27 22:53:12 -0300

    adding contacts page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import * as S from "./style";
+import logoInsta from "../../img/logo-instagram.png";
+import logoGit from "../../img/logo-github.png";
+import logoIn from "../../img/logo-linkedin.png";
+import hadouken from "../../img/heartdouken.png";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <S.Container>
+      <S.BoxTitle>
+          <h2>Meus Contatos</h2>
+      </S.BoxTitle>
+      <S.Box>
+        <S.Nav>
+          <ul>
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Portifólio</li>
+          </ul>
+        </S.Nav>
+        <S.BoxLogo>
+          <img src={logoGit} alt='' />
+          <img src={logoIn} alt='' />
+          <img src={logoInsta} alt='' />
+        </S.BoxLogo>
+        <div>
+          <img src={hadouken} alt='' />
+        </div>
+      </S.Box>
+      <div>
+        <p>feito por amor por @codebyrod</p>
+      </div>
+    </S.Container>
   )
 }
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -1,3 +1,74 @@
 //Paleta de Cores
 // #CA01D2, #70018B, #510373, #01718D, #04BFBF
 
+import styled from "styled-components";
+
+export const Container = styled.div` 
+background-color: #2e2e2e;
+height: 90vh;
+font-family: 'Red Hat Display',sans-serif;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+export const BoxTitle = styled.div` 
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+height: 20vh;
+//background-color: crimson;
+
+h2{
+    font-size: 2.3rem  ;
+}
+
+&::after{
+    content: "";
+    width: 8vw;
+    height: 1vh;
+    border-radius: 2rem;
+    background: linear-gradient(to right, #04BFBF 0%, #CA01D2 100%);
+    transition: width 700ms;
+}
+
+&:hover&::after{
+    width: 20vw;
+    }
+`
+
+export const Box = styled.div`
+width: 80vw;
+height: 30vw;
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+export const Nav = styled.nav`
+
+ul{
+    height: 20vw;
+    width: 10vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+}
+
+li{
+font-size: 1.2rem;
+}
+`
+
+export const BoxLogo = styled.div`
+display: flex;
+flex-direction: column;
+height: 15vw;
+justify-content: space-around;
+
+img{
+    width: 3vw;
+}`
+
